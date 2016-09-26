9f4746b312e86b453b0c2b287b3ada674fb15f96
Alexandre V. Palaoro <alexandre.palaoro@gmail.com>
2016-09-26 11:05:15 -0300

    Update script-size.R

--- script-size.R
+++ script-size.R
@@ -1,11 +1,19 @@
 rm(list=ls())
 
+##
+###PACKAGES NEEDED
+##
+
 library(ggplot2)
+
+##LOAD DATA
 
 size<-read.csv("size.csv",h=T)
 
-tiff(file="size-diffpop.tiff",units="mm",width=170,height=120,res=600,
-     compression="lzw")
+## Checking the differences between populatons...
+
+#tiff(file="size-diffpop.tiff",units="mm",width=170,height=120,res=600,
+#     compression="lzw")
 
 par(mfrow=c(1,2))
 hist(log(size$size.all[size$species=="floridana"&size$pop=="pop1"]),main="",
@@ -20,17 +28,24 @@ hist(log(size$size.all[size$species=="inflata"&size$pop=="pop3"]),add=T,col=adju
      prob=T)
 text(0.2,7.8,"(b)")
 
-dev.off()
+#dev.off()
+
+# Again, differences are not relevant - no need for a GLMM 
 
 par(mfrow=c(1,1))
+
+# A simple OLS will do. The size distribution is normal-ish, and the residuals 
+# are homokedastic. Thus, no reason not to use it.
+
 m1<-lm(log(size.all)~species,data=size)
 plot(m1)
 summary(m1)
 summary.aov(m1)
 
+#Plotting...
 
-tiff(file="size.tiff",units="mm",width=170,height=120,res=600,
-     compression="lzw")
+#tiff(file="size.tiff",units="mm",width=170,height=120,res=600,
+#     compression="lzw")
 par(bty='l')
 plot(log(size.all)~species,data=size,las=1,ylab="Cephalothorax width (log)",
      xlab="Species",axes=F)
@@ -39,8 +54,9 @@ axis(1,at=c(1,2,3),labels=c(expression(italic("Atlantoscia floridana")),
                                   expression(italic("Atlantoscia petronioi"))))
 axis(2,las=1)
 box()
-dev.off()
+#dev.off()
 
+# Estimating coefficients...
 m1.I<-lm(log(size.all)~species-1,data=size)
 summary(m1.I)
 m1.ci<-confint(m1.I)
@@ -49,8 +65,9 @@ m1.coef<-m1.I$coefficients
 intercept<-data.frame(v1=c("floridana","inflata","petronioi"),v2=m1.ci[,1],v3=m1.coef,v4=m1.ci[,2])
 names(intercept)<-c("species","ci.l","intercept","ci.up")
 
-tiff(file="intercept-size.tiff",units="mm",width=170,height=120,res=600,
-     compression="lzw")
+#Plotting the coefficients...
+#tiff(file="intercept-size.tiff",units="mm",width=170,height=120,res=600,
+#     compression="lzw")
 ggplot(data = intercept, aes(x = species, y = intercept, ymin = ci.l, ymax = ci.up)) +
   geom_point(position = position_dodge(width = 0.2)) +
   geom_errorbar(position = position_dodge(width = 0.2), width = 0.1) +
@@ -70,4 +87,6 @@ ggplot(data = intercept, aes(x = species, y = intercept, ymin = ci.l, ymax = ci.
                    labels=c(expression(italic("A. floridana"),
                                        italic("A. inflata"),
                                        italic("A. petronioi")))) 
-dev.off()
+#dev.off()
+
+#DONE :D
